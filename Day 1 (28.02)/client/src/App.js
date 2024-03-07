29c777c49725f3496d6169ae4de15966a2d8a2dc
RohitMore123456 <134958943+RohitMore123456@users.noreply.github.com>
2024-03-07 19:27:38 +0530

    update

--- Day 1 (28.02)/client/src/App.js
+++ Day 1 (28.02)/client/src/App.js
@@ -25,6 +25,7 @@ function App(){
         <Route path='/EffectFour' element = {<EffectFour />} />
         <Route path='/EffectFour' element = {<EffectFour />} />
         
+        
       </Routes>
     </div>
   );
