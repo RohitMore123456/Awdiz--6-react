8ef304fd31754edd84ed8f61af6fe899ae2b1393
RohitMore123456 <134958943+RohitMore123456@users.noreply.github.com>
2024-03-09 11:08:06 +0530

    update

--- Day 1 (28.02)/client/src/App.js
+++ Day 1 (28.02)/client/src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/home';
 import Login from './components/login';
-import Register from './components/Register';
 import Counter from './components/02.03/counter';
 import EffectOne from './components/03.03/Effectone';
 import EffectTwo from './components/03.03/EffectTwo';
 import EffectThree from './components/03.03/EffectThree';
 import EffectFour from './components/03.03/EffectFour';
+import Register from './components/06.03/Register';
 
 
 function App(){
@@ -17,16 +17,14 @@ function App(){
       <Routes>
         <Route path='/' element = {<Home />} /> 
         <Route path='/Login' element = {<Login />} />
-        <Route path='/Register' element = {<Register />} />
         <Route path='/Counter' element = {<Counter />} />
         <Route path='/Effectone' element = {<EffectOne />} />
         <Route path='/EffectTwo' element = {<EffectTwo />} />
         <Route path='/EffectThree' element = {<EffectThree />} />
         <Route path='/EffectFour' element = {<EffectFour />} />
         <Route path='/EffectFour' element = {<EffectFour />} />
+        <Route path='/Home' element = {<Home />} />
         <Route path='/Register' element = {<Register />} />
-        
-        
       </Routes>
     </div>
   );
--- Day 1 (28.02)/client/src/components/06.03/Register.jsx
+++ Day 1 (28.02)/client/src/components/06.03/Register.jsx
@@ -1,6 +1,38 @@
+import { useState } from "react"
+
 function Register (){
+
+    const [userdata, setUserdata] = useState({name :"", email :"", password : "", confirmpassword : "" })
+    console.log(userdata, "userdata")
+
+    function handlechange(event){
+        setUserdata({...userdata, [event.target.name]: event.target.value})
+    }
+
+    async function handlesubmit(event){
+         event.preventDefault();
+         if(userdata.name && userdata.email && userdata.password && userdata.confirmpassword){
+            alert("Registration successful")
+         }else{
+            alert ("All Feilds are required")
+         }
+    }
+
     return (
-        <div>Register</div>
+        <div>
+            <h1>Register</h1>
+            <form onSubmit={handlesubmit}>
+                <label >Name :</label><br/>
+                <input required type="Text" name="name" onChange={handlechange} /><br/>
+                <label>Email :</label><br/>
+                <input required type="email" name="email" onChange={handlechange}/><br/>
+                <label>Password :</label><br/>
+                <input required type="password" name="password" onChange={handlechange}/><br/>
+                <label>Confirm Password :</label><br/>
+                <input required type="Password" name="confirmpassword" onChange={handlechange}/><br/>
+                <input type="Submit"/>
+            </form>
+        </div>
     )
 }
 
